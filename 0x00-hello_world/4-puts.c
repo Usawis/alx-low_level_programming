9350752a3481aeb95644d76d8c9b52ebab16f5df
Wisdom Usa <triplespy793@gmail.comwq>
2022-09-08 08:05:57 -0700

    Update puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
-/* This function prints a message to the user*/
+/**
+ * main -Entry Point
+ * Description: prints "\"Programming is like a multilingual puzzle"
+ * Return: 0;
+*/
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
