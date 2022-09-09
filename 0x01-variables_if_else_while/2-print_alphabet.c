ffda313ff9f84b001a65110b4e86ebb4af6063d8
Wisdom Usa <triplespy793@gmail.com>
2022-09-09 20:44:11 +0100

    Print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Description: Prints alphabets in lowercase
+ * Return: 0 - Success
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; c++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0); }
